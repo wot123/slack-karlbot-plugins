1b77b4acc0d051342822bde650d57008e077be78
Warren Markham-Bew <warren.bew@planixs.com>
2017-05-24 13:30:51 +0100

    persistent storage demo bot added

---
+++ persistbot/persistbot.erl
@@ -0,0 +1,148 @@
+-module(persistbot).
+
+-behaviour(gen_server).
+
+%% API functions
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {self}).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(ChannelId) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [ChannelId], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([ChannelId]) ->
+    slack_client:send_message(ChannelId,"persistbot: start"),
+    syn:join(slack_messages, self()),
+    Self = slack_client:get_self(),
+    {ok, #state{self = Self}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(#{<<"type">> := <<"message">>,
+              <<"channel">> :=ChannelId,
+              <<"text">> := Text}, State) ->
+    process_command(string:tokens(binary_to_list(Text)," "), ChannelId, State),
+    {noreply, State};
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+maybe_persist([Self, "persist", Key, Value], ChannelId, Self) ->
+    karlbot_db:put(Key, Value),
+    slack_client:send_message(ChannelId, Key ++ " saved.");
+maybe_persist(_, _, _) ->
+    ok.
+
+maybe_showvalue([Self, "showvalue", Key], ChannelId, Self) ->
+    Data = karlbot_db:get(Key),
+    slack_client:send_term(ChannelId, Data);
+maybe_showvalue(_, _, _)->
+    ok.
+
+process_command(Text, ChannelId, State) ->
+    Handle = slack_client:get_handle(State#state.self),
+    maybe_persist(Text, ChannelId, Handle),
+    maybe_showvalue(Text, ChannelId, Handle).
